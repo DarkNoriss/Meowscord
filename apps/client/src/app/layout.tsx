c7eeb5aee5841ddf5963e09f342b4ee463c6c9d9
Patryk Przybylski <darknoriss@gmail.com>
2023-10-05 02:59:22 +0200

    init

---
+++ apps/client/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import "@/styles/global.css";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
