a1218c58732a175eaf283a39cbd9f397645d5b78
Patryk Przybylski <darknoriss@gmail.com>
2023-10-25 01:08:50 +0200

    feat: fix styling, add dmlist

--- apps/client/src/components/layout/navigation/NavbarFrends.tsx
+++ apps/client/src/components/layout/navigation/NavbarFrends.tsx
@@ -1,14 +1,16 @@
+import DMList from '@/components/page/friends/DMList';
 import { Button } from '@/components/ui/Button';
 
 const NavbarFrends = () => {
   return (
-    <nav className="flex w-[232px] flex-col">
-      <div className="flex h-12 items-center justify-center border-b-[1px] border-b-primary bg-gray-800 px-[10px]">
-        <Button className="h-7 w-full bg-gray-900 px-[6px] py-[1px] text-sm text-muted-foreground">
+    <nav className="flex w-60 flex-col">
+      <div className="flex h-12 items-center border-b-[1px] border-b-primary bg-gray-800 px-[10px]">
+        <Button className="h-7 w-full justify-start bg-gray-900 px-[6px] py-[1px] text-sm text-muted-foreground">
           Find or start a conversation
         </Button>
       </div>
-      <div className="flex-1 bg-gray-800" />
+
+      <DMList />
     </nav>
   );
 };
