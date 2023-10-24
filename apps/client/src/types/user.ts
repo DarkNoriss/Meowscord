142864da235776e1ce9f291caeb4720b823d0868
Patryk Przybylski <darknoriss@gmail.com>
2023-10-25 01:02:27 +0200

    feat: type for user, statuses and activities

---
+++ apps/client/src/types/user.ts
@@ -0,0 +1,34 @@
+export enum UserStatuses {
+  Online = 'online',
+  Idle = 'idle',
+  Offline = 'offline',
+  Mobile = 'mobile',
+}
+enum ActivityTypes {
+  Playing = 'playing',
+  Streaming = 'streaming',
+  Listening = 'listening',
+  Watching = 'watching',
+}
+
+type UserActivity = {
+  type: ActivityTypes;
+  name: string;
+};
+
+// type VoiceStatus = {
+//   mute?: boolean;
+//   deaf?: boolean;
+//   serverMute?: boolean;
+//   serverDeaf?: boolean;
+// };
+
+export type UserType = {
+  id: string;
+  name: string;
+  username: string;
+  bio?: string;
+  avatar?: string;
+  status: UserStatuses;
+  activity?: UserActivity;
+};
