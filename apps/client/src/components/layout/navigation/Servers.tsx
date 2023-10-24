2a27c71ec91f1cdded806065c18e71eb7c55a0c2
Patryk Przybylski <darknoriss@gmail.com>
2023-10-25 01:09:12 +0200

    feat: display servers using fake data

--- apps/client/src/components/layout/navigation/Servers.tsx
+++ apps/client/src/components/layout/navigation/Servers.tsx
@@ -1,10 +1,14 @@
+import { generateFakeServers } from '@/lib/mock';
+import type { ServerType } from '@/types/server';
+
 import Server from './Server';
 
 const Servers = () => {
+  const servers: ServerType[] = generateFakeServers();
   return (
     <>
-      {Array.from({ length: 10 }, (_, index) => (
-        <Server key={index} />
+      {servers.map((server) => (
+        <Server key={server.id} serverdata={server} />
       ))}
     </>
   );
