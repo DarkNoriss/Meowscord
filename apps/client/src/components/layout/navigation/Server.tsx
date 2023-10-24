04d36e8878fd9b862c5ce08145cfb226486c1e2a
Patryk Przybylski <darknoriss@gmail.com>
2023-10-25 01:09:28 +0200

    feat: display server

--- apps/client/src/components/layout/navigation/Server.tsx
+++ apps/client/src/components/layout/navigation/Server.tsx
@@ -1,7 +1,27 @@
-const Server = () => {
+import Image from 'next/image';
+import type { FC } from 'react';
+
+import { Button } from '@/components/ui/Button';
+import type { ServerType } from '@/types/server';
+
+type ServerProps = {
+  serverdata: ServerType;
+};
+
+const Server: FC<ServerProps> = ({ serverdata }) => {
+  const { name, photo } = serverdata;
+
   return (
     <div className="mb-2">
-      <div className="h-12 w-12 rounded-full bg-slate-50 hover:rounded-2xl" />
+      <Button className="h-12 w-12 rounded-full p-0 hover:rounded-2xl">
+        <Image
+          className="rounded-full hover:rounded-2xl"
+          src={photo}
+          alt={name}
+          height={64}
+          width={64}
+        />
+      </Button>
     </div>
   );
 };
