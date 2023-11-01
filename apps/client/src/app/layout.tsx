bbe561e11aa2cbc323c573b298b7fe7f9589ffb2
Patryk Przybylski <darknoriss@gmail.com>
2023-11-01 23:44:52 +0100

    style

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -4,6 +4,8 @@ import '@mantine/core/styles.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { cn } from '@/lib/utils';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,7 +20,14 @@ type RootLayoutProps = {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'antialiased min-h-screen bg-foreground',
+          inter.className,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 };
