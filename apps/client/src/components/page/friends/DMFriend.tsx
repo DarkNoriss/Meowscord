985d3888c8f16cecdada2f0636443ccc04c70aea
Patryk Przybylski <darknoriss@gmail.com>
2023-11-01 23:45:45 +0100

    fix: style, display name

--- apps/client/src/components/page/friends/DMFriend.tsx
+++ apps/client/src/components/page/friends/DMFriend.tsx
@@ -8,23 +8,21 @@ type DMFriendProps = {
 };
 
 const DMFriend: FC<DMFriendProps> = ({ frienddata }) => {
-  const { username, avatar } = frienddata;
+  const { name, avatar } = frienddata;
 
   return (
-    <div className="flex h-[42px] items-center py-1">
-      <div className="pl-3">
+    <div className="flex h-[42px] items-center px-3 py-1">
+      <div className="shrink-0">
         <Image
-          className="mr-2 rounded-full"
+          className="mr-3 h-8 w-8 rounded-full"
           src={avatar!}
           height={32}
           width={32}
-          alt={`${username}`}
+          alt={name}
         />
       </div>
 
-      <span className="truncate text-base text-muted-foreground">
-        {username}
-      </span>
+      <span className="truncate text-base text-muted-foreground">{name}</span>
     </div>
   );
 };
