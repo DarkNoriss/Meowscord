1d1acd58f822f230f08ed4585237da78e1fc6f92
Patryk Przybylski <darknoriss@gmail.com>
2023-11-02 07:47:13 +0100

    feat: friends filter base on status

--- apps/client/src/components/page/friends/FriendsList.tsx
+++ apps/client/src/components/page/friends/FriendsList.tsx
@@ -6,6 +6,7 @@ import Header from '@/components/layout/Header';
 import { t } from '@/lib/i18n';
 import { cn } from '@/lib/utils';
 import { useAsideStore } from '@/stores/AsideStore';
+import { useFiltersStore } from '@/stores/FiltersStore';
 import { useFriendsStore } from '@/stores/FriendsStore';
 import { useNavbarStore } from '@/stores/NavbarStore';
 import { type UserType } from '@/types/user';
@@ -29,7 +30,7 @@ const FriendButton = ({ friendData }: { friendData: UserType }) => {
           <span className="text-base font-semibold text-primary-foreground">
             {name}
           </span>
-          <span className="text-sm text-muted">
+          <span className="text-sm text-muted-foreground">
             {t(`user.status.${status}`)}
           </span>
         </div>
@@ -45,9 +46,116 @@ const FriendsList = () => {
 
   const friends = useFriendsStore((state) => state.friends);
 
-  const onlineFriends = friends
-    ?.filter((friend) => friend.status !== 'offline')
-    .sort((a, b) => a.name.localeCompare(b.name));
+  const filter = useFiltersStore((state) => state.filter);
+
+  const renderFriends = () => {
+    let label: string = '';
+    let filteredFriends: UserType[] = [];
+
+    switch (filter) {
+      case 'Online': {
+        if (friends)
+          filteredFriends = friends
+            ?.filter(
+              (friend) =>
+                friend.status !== 'offline' &&
+                friend.status !== 'pending' &&
+                friend.status !== 'blocked',
+            )
+            .sort((a, b) => a.name.localeCompare(b.name));
+
+        if (filteredFriends.length === 0) {
+          label = 'There are no online friends';
+          break;
+        }
+
+        label = `ONLINE - ${filteredFriends.length}`;
+        break;
+      }
+
+      case 'All': {
+        if (friends)
+          filteredFriends = friends
+            ?.filter(
+              (friend) =>
+                friend.status !== 'pending' && friend.status !== 'blocked',
+            )
+            .sort((a, b) => a.name.localeCompare(b.name));
+
+        if (filteredFriends.length === 0) {
+          label = 'There are no friends';
+          break;
+        }
+
+        label = `ALL FRIENDS - ${filteredFriends.length}`;
+        break;
+      }
+
+      case 'Pending': {
+        if (friends)
+          filteredFriends = friends
+            ?.filter((friend) => friend.status === 'pending')
+            .sort((a, b) => a.name.localeCompare(b.name));
+
+        if (filteredFriends.length === 0) {
+          label = 'There are no pending friend requests';
+          break;
+        }
+
+        label = `PENDING - ${filteredFriends.length}`;
+        break;
+      }
+      case 'Blocked': {
+        if (friends)
+          filteredFriends = friends
+            ?.filter((friend) => friend.status === 'blocked')
+            .sort((a, b) => a.name.localeCompare(b.name));
+
+        if (filteredFriends.length === 0) {
+          label = 'There are no blocked friends';
+          break;
+        }
+
+        label = `BLOCKED - ${filteredFriends.length}`;
+        break;
+      }
+      case 'Add Friend': {
+        if (filteredFriends.length === 0) {
+          label = 'There are no friends to be added';
+          break;
+        }
+
+        label = `ADD FRIEND - ${filteredFriends.length}`;
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+
+    return filteredFriends.length > 0 ? (
+      <>
+        <div className="ml-[30px] mr-5 mt-4 pb-2">
+          <h2 className="text-xs font-semibold text-muted-foreground">
+            {label}
+          </h2>
+        </div>
+        <div className="mt-2 pb-2 pr-4">
+          {filteredFriends?.map((friend) => (
+            <FriendButton
+              key={`friendButton-${friend.id}`}
+              friendData={friend}
+            />
+          ))}
+        </div>
+      </>
+    ) : (
+      <div className="flex h-full items-center justify-center">
+        <span className="text-xl text-muted-foreground">{label}</span>
+      </div>
+    );
+  };
 
   return (
     <div
@@ -58,22 +166,10 @@ const FriendsList = () => {
     >
       <Header />
       <div className="flex flex-1">
-        <main className="flex flex-1 flex-row bg-gray-600">
+        <main className="flex flex-1 flex-row bg-background/20">
           <div className="flex-1 border-r border-white/10">
-            <div className="mb-2 ml-[30px] mr-5 mt-4">SEARCH BUTTON</div>
-            <div className="ml-[30px] mr-5 mt-4 pb-2">
-              <h2 className="text-xs font-semibold">
-                ONLINE - {onlineFriends ? onlineFriends.length : '0'}
-              </h2>
-            </div>
-            <div className="mt-2 pb-2 pr-4">
-              {onlineFriends?.map((friend) => (
-                <FriendButton
-                  key={`friendButton-${friend.id}`}
-                  friendData={friend}
-                />
-              ))}
-            </div>
+            {/* <div className="mb-2 ml-[30px] mr-5 mt-4">SEARCH BUTTON</div> */}
+            {renderFriends()}
           </div>
           <aside className="w-[360px] p-4 pr-2">
             <h2 className="mb-4 mt-2 text-xl font-extrabold text-primary-foreground">
