de394796f268a0f51afec00f92df53df55867c53
Patryk Przybylski <darknoriss@gmail.com>
2023-11-03 18:19:32 +0100

    style

--- apps/client/src/components/layout/navigation/Navbar.tsx
+++ apps/client/src/components/layout/navigation/Navbar.tsx
@@ -3,7 +3,7 @@ import Servers from './Servers';
 
 const Navbar = () => {
   return (
-    <nav className="flex w-[72px] flex-col items-center bg-primary">
+    <nav className="flex w-[72px] flex-col items-center bg-background/10">
       <Logo />
       <Servers />
     </nav>
