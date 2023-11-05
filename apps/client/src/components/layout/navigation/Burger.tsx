faf70830904b4cd1456b24c6886af821cf600c72
Patryk Przybylski <darkoriss@gmail.com>
2023-11-06 00:56:35 +0100

    fix: use button component

--- apps/client/src/components/layout/navigation/Burger.tsx
+++ apps/client/src/components/layout/navigation/Burger.tsx
@@ -2,19 +2,21 @@
 
 import { Menu } from 'lucide-react';
 
+import { Button } from '@/components/ui/Button';
 import { useNavbarStore } from '@/stores/NavbarStore';
 
 const Burger = () => {
   const toggleNavbar = useNavbarStore((state) => state.toggleOpen);
 
   return (
-    <button
-      className="block h-full rounded active:bg-gray-600 sm:hidden"
+    <Button
+      className="m-0 mx-1 block h-8 w-8 rounded p-0 active:bg-gray-600"
       type="button"
       onClick={toggleNavbar}
+      variant="ghost"
     >
-      <Menu className="mx-1" color="#9ca3af" />
-    </button>
+      <Menu className="h-6 w-6" color="#9ca3af" />
+    </Button>
   );
 };
 
