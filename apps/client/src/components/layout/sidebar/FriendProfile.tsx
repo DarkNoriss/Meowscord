0db14be348a81769db786fa958535586c9195c20
Patryk Przybylski <darkoriss@gmail.com>
2023-11-06 01:05:58 +0100

    feat: use button component

--- apps/client/src/components/layout/sidebar/FriendProfile.tsx
+++ apps/client/src/components/layout/sidebar/FriendProfile.tsx
@@ -1,18 +1,19 @@
 import { User2 } from 'lucide-react';
 
+import { Button } from '@/components/ui/Button';
 import { useAsideStore } from '@/stores/AsideStore';
 
 const FriendProfile = () => {
   const toggleAside = useAsideStore((state) => state.toggleOpen);
 
   return (
-    <button
+    <Button
       className="h-full rounded active:bg-gray-600"
       type="button"
       onClick={toggleAside}
     >
       <User2 className="mx-1" color="#9ca3af" />
-    </button>
+    </Button>
   );
 };
 
