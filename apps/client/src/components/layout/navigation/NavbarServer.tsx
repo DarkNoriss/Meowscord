eb919ff230fb1f2bbc3d75f13a5032587ddf8000
Patryk Przybylski <darknoriss@gmail.com>
2023-11-08 16:54:35 +0100

    feat: style, props

--- apps/client/src/components/layout/navigation/NavbarServer.tsx
+++ apps/client/src/components/layout/navigation/NavbarServer.tsx
@@ -1,37 +1,44 @@
-"use client"
+'use client';
 
 import Image from 'next/image';
-import type { FC } from 'react';
+import Link from 'next/link';
+import type { ComponentPropsWithoutRef, FC } from 'react';
 
-import type { ServerType } from '@/types/server';
+import { cn } from '@/lib/utils';
 
 type NavbarServerProps = {
   tooltipContent?: string;
-  image?: {
+  image: {
     src: string;
     alt: string;
-  }
+  };
 } & ComponentPropsWithoutRef<typeof Link>;
 
-const NavbarServer: FC<NavbarServerProps> = ({ tooltipContent, image }) => {
+const NavbarServer: FC<NavbarServerProps> = ({ image, ...props }) => {
   const isActive = () => {
-    return false
-  }
+    return false;
+  };
 
-    const roundClasses = isActive
-    ? "rounded-2xl"
-    : "rounded-full hover:rounded-2xl";
+  const roundClasses = isActive()
+    ? 'rounded-2xl'
+    : 'rounded-full hover:rounded-2xl';
 
   return (
-      <Link className={cn("h-12 w-12 rounded-full p-0 hover:rounded-2xl", roundClasses)} {...props}>
-        <Image
-          className={roundClasses}
-          height={48}
+    <Link
+      className={cn(
+        'h-12 w-12 rounded-full p-0 hover:rounded-2xl',
+        roundClasses,
+      )}
+      {...props}
+    >
+      <Image
+        className={roundClasses}
+        height={48}
         width={48}
-        src={image.url}
-              alt={image.alt}
-        />
-      </Link>
+        src={image.src}
+        alt={image.alt}
+      />
+    </Link>
   );
 };
 
--- apps/client/src/components/layout/navigation/NavbarServers.tsx
+++ apps/client/src/components/layout/navigation/NavbarServers.tsx
@@ -2,22 +2,25 @@
 
 import { useEffect } from 'react';
 
+import NavbarServer from '@/components/layout/navigation/NavbarServer';
 import { generateFakeServers } from '@/lib/mock';
 import { useServersStore } from '@/stores/ServersStore';
-
-import Server from './Server';
 
 const NavbarServers = () => {
   const servers = useServersStore((state) => state.servers);
   const setServers = useServersStore((state) => state.setServers);
-
 
   useEffect(() => {
     setServers(generateFakeServers());
   }, [setServers]);
 
   return servers?.map((server) => (
-    <Server key={server.id} serverdata={server} />
+    <NavbarServer
+      key={server.id}
+      href={`/channels/${server.id}`}
+      tooltipContent={server.name}
+      image={{ alt: server.name, src: server.photo }}
+    />
   ));
 };
 
