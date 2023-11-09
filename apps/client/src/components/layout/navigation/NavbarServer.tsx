8b6f4996f5e578fc5a53ae64c8aa973c49b4fe10
Patryk Przybylski <darkoriss@gmail.com>
2023-11-09 10:43:58 +0100

    feat: tooltip

--- apps/client/src/components/layout/navigation/NavbarServer.tsx
+++ apps/client/src/components/layout/navigation/NavbarServer.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import type { ComponentPropsWithoutRef, FC } from 'react';
 
+import { TooltipProvider } from '@/components/ui/Tooltip';
 import { cn } from '@/lib/utils';
 
 type NavbarServerProps = {
@@ -24,21 +25,23 @@ const NavbarServer: FC<NavbarServerProps> = ({ image, ...props }) => {
     : 'rounded-full hover:rounded-2xl';
 
   return (
-    <Link
-      className={cn(
-        'h-12 w-12 rounded-full p-0 hover:rounded-2xl mb-2',
-        roundClasses,
-      )}
-      {...props}
-    >
-      <Image
-        className={roundClasses}
-        height={48}
-        width={48}
-        src={image.src}
-        alt={image.alt}
-      />
-    </Link>
+    <TooltipProvider>
+      <Link
+        className={cn(
+          'h-12 w-12 rounded-full p-0 hover:rounded-2xl mb-2',
+          roundClasses,
+        )}
+        {...props}
+      >
+        <Image
+          className={roundClasses}
+          height={48}
+          width={48}
+          src={image.src}
+          alt={image.alt}
+        />
+      </Link>
+    </TooltipProvider>
   );
 };
 
