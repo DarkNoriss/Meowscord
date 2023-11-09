73714b90033d565669eaa9610258faa4ea4f71ab
Patryk Przybylski <darkoriss@gmail.com>
2023-11-09 10:43:48 +0100

    rename component

--- apps/client/src/components/layout/navigation/Navbar.tsx
+++ apps/client/src/components/layout/navigation/Navbar.tsx
@@ -1,11 +1,11 @@
-import Logo from '@/components/layout/navigation/Logo';
 import NavbarCreateServer from '@/components/layout/navigation/NavbarCreateServer';
+import NavbarLogo from '@/components/layout/navigation/NavbarLogo';
 import NavbarServers from '@/components/layout/navigation/NavbarServers';
 
 const Navbar = () => {
   return (
     <nav className="fixed flex h-screen w-[72px] flex-col items-center overflow-y-auto bg-background/10 pt-3">
-      <Logo />
+      <NavbarLogo />
       <NavbarServers />
       <NavbarCreateServer />
     </nav>
