d5a1aa42370cf1f5484bd54273a99f2e7ecd5650
Patryk Przybylski <darkoriss@gmail.com>
2023-11-16 22:57:12 +0100

    Add publicRoutes function to authMiddleware

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -1,12 +1,15 @@
 import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import type { NextRequest } from 'next/server';
 
 export default authMiddleware({
+  publicRoutes: (req: NextRequest) => req.nextUrl.pathname === '/',
+
+  afterAuth(auth, req) {
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
     }
     return null;
   },
-  publicRoutes: ['/'],
 });
 
 export const config = {
