1c958664e722af2ca0087dc88f94f83a3f01eb7a
Patryk Przybylski <darkoriss@gmail.com>
2023-11-16 22:31:09 +0100

    Refactor user status footer component to use new Avatar and Button components

--- apps/client/src/components/layout/user-status-footer/UserStatusFooter.tsx
+++ apps/client/src/components/layout/user-status-footer/UserStatusFooter.tsx
@@ -1,7 +1,8 @@
 import { currentUser } from '@clerk/nextjs';
 import type { FC } from 'react';
 
-import ProfileAvatar from '@/components/ProfileAvatar';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/Avatar';
+import { Button } from '@/components/ui/Button';
 
 type UserStatusFooterProps = {};
 
@@ -11,16 +12,20 @@ const UserStatusFooter: FC<UserStatusFooterProps> = async () => {
   if (!user) return null;
 
   return (
-    <div className="bottom-0 flex h-[52px] flex-row justify-between bg-footer px-2">
-      <div className="flex flex-row items-center ">
-        <ProfileAvatar src={user.imageUrl} alt={user.username!} />
-        <div className="flex flex-col py-1 pl-2">
-          <span>
-            {user.firstName} {user.firstName}
-          </span>
-          <span>{user.username}</span>
+    <div className="bottom-0 flex h-[52px] flex-row items-center justify-between bg-footer px-2">
+      <Button
+        className="m-0 mr-2 flex w-full flex-row items-center justify-start p-0 pl-[2px]"
+        variant="ghost"
+      >
+        <Avatar>
+          <AvatarImage src={user.imageUrl} />
+          <AvatarFallback>{user.username}</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col items-start py-1 pl-2">
+          <span className="text-sm text-primary">{user.firstName}</span>
+          <span className="text-xs text-muted">{user.username}</span>
         </div>
-      </div>
+      </Button>
       <div>b</div>
     </div>
   );
