61510935a82facf7d9479ec00dbb9c2724e6b8e8
Patryk Przybylski <darknoriss@gmail.com>
2023-11-19 10:21:55 +0100

    Refactor CreateServer component

--- apps/client/src/components/layout/navigation/CreateServer.tsx
+++ apps/client/src/components/layout/navigation/CreateServer.tsx
@@ -1,23 +1,14 @@
-'use client';
-
 import { PlusIcon } from 'lucide-react';
 import type { FC } from 'react';
-import { useState } from 'react';
 
 import { Button } from '@/components/ui/Button';
 
 type AddServerProps = {};
 
 const AddServer: FC<AddServerProps> = () => {
-  const [hovered, setHovered] = useState(false);
-
   return (
-    <Button
-      className="m-0 h-12 w-12 rounded-full bg-navigation-button p-0 hover:rounded-2xl hover:bg-navigation-button-hover"
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <PlusIcon color={hovered ? '#FFFFFF' : '#23A559'} />
+    <Button className="m-0 h-12 w-12 rounded-full bg-navigation-button p-0 text-green-600 hover:rounded-2xl hover:bg-navigation-button-hover hover:text-primary">
+      <PlusIcon />
     </Button>
   );
 };
