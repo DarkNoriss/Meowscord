99feb14c10d37a2dcbf9cc25022c2e272463feee
Patryk Przybylski <darknoriss@gmail.com>
2023-11-19 09:58:24 +0100

    Refactor generateFakeFriends and generateFakeServers to include type annotations

--- apps/client/src/lib/mock.ts
+++ apps/client/src/lib/mock.ts
@@ -1,11 +1,13 @@
 import { faker } from '@faker-js/faker';
 
+import type { ServerType } from '@/types/server';
+import type { UserType } from '@/types/user';
 import { UserStatuses } from '@/types/user';
 
 const generateFakeId = () =>
   faker.number.int({ min: 100000000000000, max: 999999999999999 }).toString();
 
-export const generateFakeFriends = (numberOfFriends: number) => {
+export const generateFakeFriends = (numberOfFriends: number): UserType[] => {
   return Array.from({ length: numberOfFriends }, (_) => ({
     id: generateFakeId(),
     fullName: faker.person.fullName(),
@@ -15,7 +17,7 @@ export const generateFakeFriends = (numberOfFriends: number) => {
   }));
 };
 
-export const generateFakeServers = (numberOfServers: number) => {
+export const generateFakeServers = (numberOfServers: number): ServerType[] => {
   return Array.from({ length: numberOfServers }, (_) => ({
     id: generateFakeId(),
     name: faker.animal.cat(),
