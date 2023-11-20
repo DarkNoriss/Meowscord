4cae472ada496c09c6fe830aec11b432f48c3cbf
Patryk Przybylski <darkoriss@gmail.com>
2023-11-20 21:12:17 +0100

    Update DMPage component with new header and separator

--- apps/client/src/components/islets/dm-page/DMPage.tsx
+++ apps/client/src/components/islets/dm-page/DMPage.tsx
@@ -2,12 +2,19 @@ import { BsPersonFill } from 'react-icons/bs';
 
 import PageHeaderWrapper from '@/components/layout/page/PageHeaderWrapper';
 import PageWrapper from '@/components/layout/page/PageWrapper';
+import { Separator } from '@/components/ui/Separator';
 
 const DMPage = () => {
   return (
     <PageWrapper>
-      <PageHeaderWrapper>DMPage</PageHeaderWrapper>
-      <BsPersonFill />
+      <PageHeaderWrapper>
+        <BsPersonFill className="mx-2 text-muted" size={24} />
+        <span className="mr-2 text-base font-semibold text-primary">
+          Friends
+        </span>
+        <Separator className="mx-2 h-6 w-[1px]" orientation="vertical" />
+        DMPage
+      </PageHeaderWrapper>
     </PageWrapper>
   );
 };
