45df0ca4bbb4db2a3deb8c20569e367c0429384b
Patryk Przybylski <darkoriss@gmail.com>
2023-11-21 17:23:29 +0100

    Refactor DMMenuItem component to dynamically render icons and paths

--- apps/client/src/components/islets/dm-navigation/dm-menu-item.tsx
+++ apps/client/src/components/islets/dm-navigation/dm-menu-item.tsx
@@ -1,9 +1,39 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { usePathname } from 'next/navigation';
+import type { ComponentType } from 'react';
+import type { IconBaseProps } from 'react-icons';
 
 import { Button } from '@/components/ui/Button';
 import { cn } from '@/lib/utils';
+
+const IconFriend = dynamic(() =>
+  import('react-icons/bs').then((m) => m.BsPersonFill),
+);
+const IconNitro = dynamic(() => import('react-icons/fa').then((m) => m.FaCar));
+const IconStore = dynamic(() =>
+  import('react-icons/bi').then((m) => m.BiSolidStore),
+);
+type IconComponents = {
+  [key: string]: ComponentType<IconBaseProps>;
+};
+
+const iconComponents: IconComponents = {
+  Friends: IconFriend,
+  Nitro: IconNitro,
+  Store: IconStore,
+};
+
+type Paths = {
+  [key: string]: string;
+};
+
+const paths: Paths = {
+  Friends: '/channels/me',
+  Nitro: '/nitro',
+  Store: '/store',
+};
 
 type DMMenuItemProps = {
   label: string;
@@ -11,18 +41,22 @@ type DMMenuItemProps = {
 
 const DMMenuItem = ({ label }: DMMenuItemProps) => {
   const pathname = usePathname();
+  const Icon = iconComponents[label];
 
   const isActive = () => {
-    return pathname === '/channels/me';
+    return pathname === paths[label];
   };
 
   return (
-    <Button className={cn('justify-start')} variant="ghost">
-      <span
-        className={cn('text-base', isActive() ? 'text-primary' : 'text-muted')}
-      >
-        {label}
-      </span>
+    <Button
+      className={cn(
+        'justify-start',
+        isActive() ? 'text-primary' : 'text-muted',
+      )}
+      variant="ghost"
+    >
+      {Icon && <Icon className="mr-3" size={24} />}
+      <span className="text-base">{label}</span>
     </Button>
   );
 };
