40406a0ba0a7cc7cc91071d7f7f9675a4c7ff5fa
DarkNoriss <darknoriss@gmail.com>
2023-11-26 20:20:54 +0100

    Refactor GET route in server API

--- apps/client/src/app/api/servers/get/route.ts
+++ apps/client/src/app/api/servers/get/route.ts
@@ -5,16 +5,17 @@
 // import type { Server } from '@/models/schema';
 // import { servers, users } from '@/models/schema';
 
-// export async function GET() {
-//   const { userId } = auth();
+export async function GET() {
+  // const { userId } = auth();
 
-//   if (!userId) {
-//     return [] as Server[];
-//   }
-//   const result = await db
-//     .select()
-//     .from(servers)
-//     .where(sql`${users.id} = ${userId}`);
+  // if (!userId) {
+  //   return [] as Server[];
+  // }
+  // const result = await db
+  //   .select()
+  //   .from(servers)
+  //   .where(sql`${users.id} = ${userId}`);
 
-//   return new Response(JSON.stringify({ result }), { status: 200 });
-// }
+  // return new Response(JSON.stringify({ result }), { status: 200 });
+  return new Response('', { status: 200 });
+}
