ae9feb292a8057a05973bc7aeb9ad971cd6e0c2f
DarkNoriss <darknoriss@gmail.com>
2023-11-26 20:16:13 +0100

    Commented out unnecessary code

--- apps/client/src/app/api/servers/get/route.ts
+++ apps/client/src/app/api/servers/get/route.ts
@@ -1,20 +1,20 @@
-import { auth } from '@clerk/nextjs';
-import { sql } from 'drizzle-orm/sql';
+// import { auth } from '@clerk/nextjs';
+// import { sql } from 'drizzle-orm/sql';
 
-import { db } from '@/lib/db';
-import type { Server } from '@/models/schema';
-import { servers, users } from '@/models/schema';
+// import { db } from '@/lib/db';
+// import type { Server } from '@/models/schema';
+// import { servers, users } from '@/models/schema';
 
-export async function GET() {
-  const { userId } = auth();
+// export async function GET() {
+//   const { userId } = auth();
 
-  if (!userId) {
-    return [] as Server[];
-  }
-  const result = await db
-    .select()
-    .from(servers)
-    .where(sql`${users.id} = ${userId}`);
+//   if (!userId) {
+//     return [] as Server[];
+//   }
+//   const result = await db
+//     .select()
+//     .from(servers)
+//     .where(sql`${users.id} = ${userId}`);
 
-  return new Response(JSON.stringify({ result }), { status: 200 });
-}
+//   return new Response(JSON.stringify({ result }), { status: 200 });
+// }
