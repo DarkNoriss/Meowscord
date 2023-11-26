01c299522ddb324dfd1a42be0710423b6b966383
Patryk Przybylski <darknoriss@gmail.com>
2023-11-26 16:13:17 +0100

    Add API call to fetch server data

--- apps/client/src/components/islets/navbar/navbar-server-list.tsx
+++ apps/client/src/components/islets/navbar/navbar-server-list.tsx
@@ -1,9 +1,17 @@
 // import { useQuery } from '@tanstack/react-query';
 
+import axios from 'axios';
+
 // import NavbarServerItem from '@/components/islets/navbar/navbar-server-item';
 // import type { ServerType } from '@/types/server';
+const getData = async () => {
+  const data = await axios.get('/api/servers/get');
+  return { data };
+};
 
-const NavbarServerList = () => {
+const NavbarServerList = async () => {
+  const { data } = await getData();
+  console.log(data);
   // const { data } = useQuery<ServerType[]>({ queryKey: ['servers'] });
   return null;
   // return data?.map((server: ServerType) => (
