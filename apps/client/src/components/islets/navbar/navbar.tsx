7d1eee66f4e01e4546aebef5f2c05f4e617526b4
DarkNoriss <darknoriss@gmail.com>
2023-11-26 23:00:41 +0100

    Add NavbarServerListItemMigrate component

--- apps/client/src/components/islets/navbar/navbar.tsx
+++ apps/client/src/components/islets/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import '@/styles/module.css';
 
 import NavbarServerAdd from '@/components/islets/navbar/navbar-server-add';
 import NavbarServerList from '@/components/islets/navbar/navbar-server-list';
+import NavbarServerListItemMigrate from '@/components/islets/navbar/navbar-server-list-item-migrate';
 import NavbarServerLogo from '@/components/islets/navbar/navbar-server-logo';
 import NavbarWrapper from '@/components/layout/navbar/navbar-wrapper';
 import { Separator } from '@/components/ui/Separator';
@@ -15,6 +16,7 @@ const Navbar = () => {
         <Separator className="mx-auto mb-2 h-[2px] w-8" />
         <NavbarServerList />
         <NavbarServerAdd />
+        <NavbarServerListItemMigrate />
       </TooltipProvider>
     </NavbarWrapper>
   );
