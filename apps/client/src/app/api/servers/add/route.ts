1b6b32ba6dfd09eda5cab9d7f4267e4ae1a4caf0
DarkNoriss <darknoriss@gmail.com>
2023-11-26 19:49:26 +0100

    Commented out unused code in server add route

--- apps/client/src/app/api/servers/add/route.ts
+++ apps/client/src/app/api/servers/add/route.ts
@@ -3,17 +3,17 @@ import { nanoid } from 'nanoid';
 
 import { db } from '@/lib/db';
 import { createFakeServer } from '@/lib/mock';
-import { servers } from '@/models/schema';
+// import { servers } from '@/models/schema';
 
 export async function POST() {
   const id = nanoid();
   const { name } = createFakeServer();
   const { userId: ownerId } = auth();
 
-  if (!ownerId) {
-    return new Response('', { status: 200 });
-  }
-  await db.insert(servers).values({ id, name, ownerId, usersId: ownerId });
-
+  // if (!ownerId) {
+  //   return new Response('', { status: 200 });
+  // }
+  // await db.insert(servers).values({ id, name, ownerId, usersId: ownerId });
+  await db;
   return new Response('', { status: 200 });
 }
