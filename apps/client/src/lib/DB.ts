a353efb7a07ef32afe64f10e5470b4bd07a0f0b8
Patryk Przybylski <darknoriss@gmail.com>
2023-11-26 16:13:32 +0100

    Import schema and update drizzle configuration

--- apps/client/src/lib/DB.ts
+++ apps/client/src/lib/DB.ts
@@ -3,6 +3,7 @@ import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import { migrate } from 'drizzle-orm/libsql/migrator';
 
+import * as schema from '../models/schema';
 import { Env } from './Env.mjs';
 
 const client = createClient({
@@ -10,7 +11,7 @@ const client = createClient({
   authToken: Env.DATABASE_AUTH_TOKEN,
 });
 
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
 
 // Disable migrate function if using Edge runtime for local environment and use `drizzle-kit push` instead
 if (process.env.NODE_ENV !== 'production') {
