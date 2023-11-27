6523dc137cf57bb6adb244aa9555c5ec25b315f6
Patryk Przybylski <darknoriss@gmail.com>
2023-11-27 21:53:50 +0100

    Refactor server add route

--- apps/client/src/app/api/servers/add/route.ts
+++ apps/client/src/app/api/servers/add/route.ts
@@ -1,32 +1,29 @@
-// /* eslint-disable no-console */
-// import { db } from '@/lib/db';
-// import { users } from '@/models/schema';
+/* eslint-disable no-console */
 
-// export async function POST(req: Request) {
-//   try {
-//     const {
-//       id,
-//       firstName,
-//       lastName,
-//       username,
-//       imageUrl,
-//       profileImageUrl,
-//       createdAt,
-//     } = await req.json();
-
-//     await db.insert(users).values({
-//       id,
-//       firstName,
-//       lastName,
-//       username,
-//       imageUrl,
-//       profileImageUrl,
-//       createdAt,
-//     });
-
-//     return new Response('User added!', { status: 200 });
-//   } catch (error) {
-//     console.error(error);
-//     return new Response('Error adding user', { status: 500 });
-//   }
-// }
+export async function POST() {
+  // try {
+  //   const {
+  //     id,
+  //     firstName,
+  //     lastName,
+  //     username,
+  //     imageUrl,
+  //     profileImageUrl,
+  //     createdAt,
+  //   } = await req.json();
+  //   await db.insert(users).values({
+  //     id,
+  //     firstName,
+  //     lastName,
+  //     username,
+  //     imageUrl,
+  //     profileImageUrl,
+  //     createdAt,
+  //   });
+  //   return new Response('User added!', { status: 200 });
+  // } catch (error) {
+  //   console.error(error);
+  //   return new Response('Error adding user', { status: 500 });
+  // }
+  return new Response('', { status: 200 });
+}
