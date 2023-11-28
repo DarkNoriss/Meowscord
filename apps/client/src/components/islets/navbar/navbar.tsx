09617a1a4e52ae384a9c6a21c0b2333bd16b121b
Patryk Przybylski <darknoriss@gmail.com>
2023-11-28 04:59:40 +0100

    Add NavbarServerItemFriend component in development environment

--- apps/client/src/components/islets/navbar/navbar.tsx
+++ apps/client/src/components/islets/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import '@/styles/module.css';
 
 import NavbarList from '@/components/islets/navbar/navbar-list';
 import NavbarListItemAdd from '@/components/islets/navbar/navbar-list-item-add';
+import NavbarServerItemFriend from '@/components/islets/navbar/navbar-list-item-friend';
 import NavbarServerItemLogo from '@/components/islets/navbar/navbar-list-item-logo';
 import NavbarServerItemMigrate from '@/components/islets/navbar/navbar-list-item-migrate';
 import NavbarWrapper from '@/components/layout/navbar/navbar-wrapper';
@@ -9,6 +10,8 @@ import { Separator } from '@/components/ui/Separator';
 import { TooltipProvider } from '@/components/ui/Tooltip';
 
 const Navbar = () => {
+  const isDev = process.env.NODE_ENV === 'development';
+
   return (
     <NavbarWrapper>
       <TooltipProvider>
@@ -16,7 +19,12 @@ const Navbar = () => {
         <Separator className="mx-auto mb-2 h-[2px] w-8" />
         <NavbarList />
         <NavbarListItemAdd />
-        <NavbarServerItemMigrate />
+        {isDev && (
+          <>
+            <NavbarServerItemMigrate />
+            <NavbarServerItemFriend />
+          </>
+        )}
       </TooltipProvider>
     </NavbarWrapper>
   );
