3704f132fdd3318440ddd9654823d0d29e98ec1e
Patryk Przybylski <darknoriss@gmail.com>
2023-11-28 04:59:46 +0100

    Add NavbarServerItemFriend component to navbar

---
+++ apps/client/src/components/islets/navbar/navbar-list-item-friend.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+
+import { Button } from '@/components/ui/Button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/Tooltip';
+
+const NavbarServerItemFriend = () => {
+  const mutation = useMutation({
+    // mutationFn: () => {
+    //   return axios.post('/api/servers/create');
+    // },
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({ queryKey: ['servers'] });
+    // },
+  });
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button
+          className="m-0 mb-2 h-12 w-12 shrink-0 rounded-full bg-navigation-button p-0 text-green-600 hover:rounded-2xl hover:bg-navigation-button-hover hover:text-primary"
+          onClick={() => mutation.mutate()}
+        >
+          <span className="text-2xl">F</span>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent side="right" sideOffset={15}>
+        Add friend
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
+export default NavbarServerItemFriend;
