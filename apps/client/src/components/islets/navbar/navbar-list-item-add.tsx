7d26ddc91cf46e8078afd9b6e93a4a7737b6feff
Patryk Przybylski <darknoriss@gmail.com>
2023-12-01 17:42:17 +0100

    Fix submit button onClick event

--- apps/client/src/components/islets/navbar/navbar-list-item-add.tsx
+++ apps/client/src/components/islets/navbar/navbar-list-item-add.tsx
@@ -26,7 +26,7 @@ const NavbarListItemAdd = () => {
         <form action={handleClick}>
           <Button
             className="m-0 mb-2 h-12 w-12 shrink-0 rounded-full bg-navigation-button p-0 text-green-600 hover:rounded-2xl hover:bg-navigation-button-hover hover:text-primary"
-            type="submit" // onClick={() => mutation.mutate()}
+            type="submit"
           >
             <LuPlus size={24} />
           </Button>
