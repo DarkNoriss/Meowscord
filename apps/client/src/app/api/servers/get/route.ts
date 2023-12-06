71f9dcf965a180cf2b4ff0ab98d3aad305a03dba
Patryk Przybylski <darknoriss@gmail.com>
2023-12-06 16:42:10 +0100

    Add type import for ServerWithoutOwner

--- apps/client/src/app/api/servers/get/route.ts
+++ apps/client/src/app/api/servers/get/route.ts
@@ -3,6 +3,7 @@ import { eq } from 'drizzle-orm/sql';
 
 import { db } from '@/db';
 import { servers, userServers } from '@/db/schema';
+import type { ServerWithoutOwner } from '@/types/server';
 
 export async function GET() {
   const { userId } = auth();
@@ -11,12 +12,11 @@ export async function GET() {
     return new Response('User not authenticated', { status: 401 });
   }
 
-  const serversData = await db
+  const serversData = (await db
     .select({ id: servers.id, name: servers.name })
     .from(userServers)
     .leftJoin(servers, eq(servers.id, userServers.serverId))
-    .where(eq(userServers.userId, userId))
-    .then((data) => (Array.isArray(data) ? data : [data]));
+    .where(eq(userServers.userId, userId))) as ServerWithoutOwner[];
 
   return new Response(JSON.stringify(serversData), { status: 200 });
 }
