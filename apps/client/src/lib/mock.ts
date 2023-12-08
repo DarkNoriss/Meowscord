786b9de52cde2860e186c6e9082edfdd3b72d4e0
DarkNoriss <darknoriss@gmail.com>
2023-12-08 07:52:38 +0100

    Refactor mock.ts file to remove unused code

--- apps/client/src/lib/mock.ts
+++ apps/client/src/lib/mock.ts
@@ -2,21 +2,10 @@ import { faker } from '@faker-js/faker';
 import { nanoid } from 'nanoid';
 
 import { statusEnum } from '@/db/schema';
-import type { User, UserType } from '@/types/user';
-import { UserStatuses } from '@/types/user';
+import type { User } from '@/types/user';
 
 const generateFakeId = () =>
   faker.number.int({ min: 100000000000000, max: 999999999999999 }).toString();
-
-export const generateFakeFriends = (numberOfFriends: number): UserType[] => {
-  return Array.from({ length: numberOfFriends }, (_) => ({
-    id: generateFakeId(),
-    fullName: faker.person.fullName(),
-    username: faker.internet.userName(),
-    avatar: faker.image.avatarGitHub(),
-    status: faker.helpers.arrayElement(Object.values(UserStatuses)),
-  }));
-};
 
 export const generateFakeUser = (): User => {
   return {
