fce4852c71ddb177e5de9e21e8a10922843257e9
DarkNoriss <darknoriss@gmail.com>
2023-12-08 08:16:23 +0100

    Fix type in page.tsx

--- apps/client/src/app/channels/[id]/page.tsx
+++ apps/client/src/app/channels/[id]/page.tsx
@@ -5,18 +5,18 @@ import { redirect } from 'next/navigation';
 
 import ChannelNavmenu from '@/components/islets/channel-navmeu/channel-navmenu';
 import ChannelPage from '@/components/islets/channel-page/channel-page';
-import type { ServerType } from '@/types/server';
+import type { Server } from '@/types/server';
 
 type PageProps = {
   params: { id: string };
 };
 
 const Page = ({ params: { id } }: PageProps) => {
-  const { data } = useQuery<ServerType[]>({ queryKey: ['servers'] });
+  const { data } = useQuery<Server[]>({ queryKey: ['servers'] });
 
   if (!data) redirect('/channels/me');
 
-  const channelData = data.find((server: ServerType) => server.id === id);
+  const channelData = data.find((server: Server) => server.id === id);
 
   if (!channelData) redirect('/channels/me');
 
