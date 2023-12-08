a636807f1ff8588ec738c7bfda16e130aae6e6fa
Patryk Przybylski <darknoriss@gmail.com>
2023-12-08 03:14:56 +0100

    Add User type to friends API response

--- apps/client/src/app/api/friends/get/route.ts
+++ apps/client/src/app/api/friends/get/route.ts
@@ -3,6 +3,7 @@ import { eq } from 'drizzle-orm/sql';
 
 import { db } from '@/db';
 import { userFriends, users } from '@/db/schema';
+import type { User } from '@/types/user';
 
 export async function GET() {
   const { userId } = auth();
@@ -11,11 +12,19 @@ export async function GET() {
     return new Response('User not authenticated', { status: 401 });
   }
 
-  const friendsData = await db
-    .select()
+  const friendsData = (await db
+    .select({
+      id: users.id,
+      firstName: users.firstName,
+      lastName: users.lastName,
+      username: users.username,
+      imageUrl: users.imageUrl,
+      createdAt: users.createdAt,
+      status: users.status,
+    })
     .from(userFriends)
-    .leftJoin(users, eq(users.id, userFriends.userId))
-    .where(eq(userFriends.userId, userId));
+    .innerJoin(users, eq(users.id, userFriends.friendId))
+    .where(eq(userFriends.userId, userId))) as User[];
 
   return new Response(JSON.stringify(friendsData), { status: 200 });
 }
