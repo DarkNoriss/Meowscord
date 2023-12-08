e30e94993761238ae918d5b844b2326b84c4d83d
Patryk Przybylski <darknoriss@gmail.com>
2023-12-08 03:16:29 +0100

    Refactor user.ts: Remove unused code and export types
    This commit refactors the user.ts file by removing unused code, such as the UserStatuses enum, ActivityTypes enum, and UserActivity type. It also exports the UserType type and UserStatus type.

--- apps/client/src/types/user.ts
+++ apps/client/src/types/user.ts
@@ -1,44 +1,5 @@
 import type { statusEnum, users } from '@/db/schema';
 
-export enum UserStatuses {
-  Online = 'online',
-  Idle = 'idle',
-  DND = 'dnd',
-  Offline = 'offline',
-  Mobile = 'mobile',
-  Pending = 'pending',
-  Blocked = 'blocked',
-}
-
-enum ActivityTypes {
-  Playing = 'playing',
-  Streaming = 'streaming',
-  Listening = 'listening',
-  Watching = 'watching',
-}
-
-type UserActivity = {
-  type: ActivityTypes;
-  name: string;
-};
-
-// type VoiceStatus = {
-//   mute?: boolean;
-//   deaf?: boolean;
-//   serverMute?: boolean;
-//   serverDeaf?: boolean;
-// };
-
-export type UserType = {
-  id: string;
-  fullName: string;
-  username: string;
-  bio?: string;
-  avatar: string;
-  status: UserStatuses;
-  activity?: UserActivity;
-};
-
 export type User = typeof users.$inferSelect;
 
 export type UserStatus = typeof statusEnum.enumValues;
